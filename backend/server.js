146753c3861d904e1587240a01645638ebf45bde
masterchinchilla <47091399+masterchinchilla@users.noreply.github.com>
2022-02-08 21:59:06 -0600

    started

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+import weekMealPlans from "./api/weekMealPlans.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/weekMealPlans", weekMealPlans)
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
+
+export default app
