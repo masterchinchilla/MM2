b9f2427bb0dbbb1f45c1a85abc147bdc0f029635
masterchinchilla <47091399+masterchinchilla@users.noreply.github.com>
2023-05-26 14:52:23 -0500

    After remove legacy WMP & Day code in WMP Parent Cmpnt

--- macro-menu/src/components/WeekMealPlanPage.component.jsx
+++ macro-menu/src/components/WeekMealPlanPage.component.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import TabNav from "./TabNav.component";
 import { Player } from "@lottiefiles/react-lottie-player";
-// import NewWeekMealPlanCard from "./NewWeekMealPlanCard.component";
-// import CustomHeading from "./CustomHeading.component";
-// import Day from "./Day.component";
 import ShoppingList from "./ShoppingList.component";
 import SpreadsheetView from "./SpreadsheetView.component";
 import LoadingForkSpoonSpinner from "../assets/lottieForkSpoonSpinner.json";
@@ -158,116 +155,6 @@ const WeekMealPlanPage = (props) => {
             },
           }}
         />
-        {/* <NewWeekMealPlanCard
-          key={`NewWeekMealPlanCard_for_WMP_${thisWMPRecordId}`}
-          commonProps={{
-            commonData: { backEndHtmlRoot: backEndHtmlRoot },
-            commonMethods: {
-              getRndIntegerFn: getRndIntegerFn,
-              returnElementKey: returnElementKey,
-              onCreateNewRecordFn: onCreateNewRecordFn,
-              onUpdatePropFn: onUpdateWMPPropFn,
-              onSaveChangesFn: onSaveChangesFn,
-              onStartEditingFn: onStartEditingFn,
-              onCancelEditFn: onCancelEditFn,
-              onDeleteObjFn: onDeleteObjFn,
-              onSrchDBForObjWMtchngNmeFn: onGetRecordsWFilterFn,
-              trimEnteredValueFn: trimEnteredValueFn,
-              onCopyWMPFn: onCopyWMPFn,
-            },
-          }}
-          specificProps={{
-            specificData: {
-              thisStateObj: thisWMPStateObj,
-              thisStateObjBackup: thisWMPStateBackup,
-            },
-            specificMethods: {
-              onUpdateWeightsFn: onUpdateWeightsFn,
-            },
-          }}
-        />
-        <div className="card mt-3 mb-3">
-          <div className="card-header">
-            <CustomHeading
-              key={`CustomHeading_"Day Meal Plans"_for_WMP_${thisWMPRecordId}`}
-              headingLvl={2}
-              recordLoaded={wmpRecordLoaded}
-              headingText="Day Meal Plans"
-              hdngIsReqFormLbl={false}
-              editingForm={false}
-              headingClasses="card-title"
-            />
-          </div>
-          <div className="card-body">
-            <div
-              className="accordion accordion-flush"
-              id={"accordionFull" + thisWMPRecordId}
-            >
-              <div className="accordion-item">
-                <h2
-                  className="accordion-header"
-                  id={"accordionHeader" + thisWMPRecordId}
-                >
-                  <button
-                    className="accordion-button"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target={"#dayAccrdn" + thisWMPRecordId}
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                  ></button>
-                </h2>
-              </div>
-              <div
-                id={"dayAccrdn" + thisWMPRecordId}
-                className="accordion-collapse collapse show"
-                aria-labelledby={"#accordionHeader" + thisWMPRecordId}
-                data-bs-parent={"#accordionFull" + thisWMPRecordId}
-              >
-                <div className="accordion-body wkDaysAccrdnBdy">
-                  <Day
-                    commonProps={{
-                      commonData: {
-                        backEndHtmlRoot: backEndHtmlRoot,
-                        daysOfWeek: daysOfWeek,
-                        mealTypes: mealTypes,
-                        allUnitOfMeasures: allUnitOfMeasures,
-                        allWeightTypes: allWeightTypes,
-                        allBrands: allBrands,
-                        allGenRecipes: allGenRecipes,
-                      },
-                      commonMethods: {
-                        getRndIntegerFn: getRndIntegerFn,
-                        returnElementKey: returnElementKey,
-                        onGetRecordsWFilterFn: onGetRecordsWFilterFn,
-                        onCreateNewRecordFn: onCreateNewRecordFn,
-                        onUpdatePropFn: onUpdateMealOrChildPropFn,
-                        onSaveChangesFn: onSaveChangesFn,
-                        onStartEditingFn: onStartEditingFn,
-                        onCancelEditFn: onCancelEditFn,
-                        onDeleteObjFn: onDeleteObjFn,
-                        trimEnteredValueFn: trimEnteredValueFn,
-                      },
-                    }}
-                    specificProps={{
-                      specificData: {
-                        thisDayOfWeekName: `Sunday`,
-                        wmpUserType: thisWMPStateObj.userType.weekMealPlan,
-                        thisDayStateObj: sunday,
-                        thisDayStateObjBackup: sundayBackup,
-                      },
-                      specificMethods: {
-                        populateMissingMealIngrdnts:
-                          populateMissingMealIngrdnts,
-                        onAddIngrdntToRecipeFn: onAddIngrdntToRecipeFn,
-                      },
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
       <div
         className="container-fluid pl-4 pr-4"
